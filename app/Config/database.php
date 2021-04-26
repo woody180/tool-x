a421279645853217d468a7c58fa7ac056ebb78bd
= <=>
2021-04-26 15:53:44 +0400

    clear database config file

--- app/Config/database.php
+++ app/Config/database.php
@@ -4,8 +4,8 @@ define("DATABASE", FALSE);
 define('DATABASE_TYPE', 'MySQL'); // MySQL, PostgreSQL, SQLite, CUBRID
 
 CONST DB_HOST       = 'localhost';
-CONST DB_NAME       = 'lesson';
-CONST DB_USERNAME   = 'root';
+CONST DB_NAME       = '';
+CONST DB_USERNAME   = '';
 CONST DB_PASSWORD   = '';
 CONST DB_PORT       = '30000';
 CONST SQLITE_PATH   = '';
