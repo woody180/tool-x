7ccc1e0f3e232ef6f6f04027f56f93e2902bb755
= <=>
2021-04-30 11:24:48 +0400

    config files

--- app/Config/app.php
+++ app/Config/app.php
@@ -2,3 +2,5 @@
 define("APPNAME", "APPName");
 define("APPEMAIL", NULL);
 define("ERROR_HANDLING", true);
+date_default_timezone_set("Asia/Tbilisi"); // https://www.php.net/manual/en/timezones.php
+define("FORCE_SECURE_REQUESTS", FALSE);
--- app/Config/urls.php
+++ app/Config/urls.php
@@ -5,6 +5,4 @@
 define("URLROOT", (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]");
 define("PUBLIC_DIR", URLROOT . "/assets");
 define("APPROOT", dirname(dirname(__FILE__)));
-define("FORCE_SECURE_REQUESTS", FALSE);
 define("CURRENT_URL", (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
-date_default_timezone_set("Asia/Tbilisi"); // https://www.php.net/manual/en/timezones.php
