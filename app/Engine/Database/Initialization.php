8ec16d524c83bd64dd47eb08bdaafe5494dcb6c0
= <=>
2021-09-18 13:49:21 +0400

    fix db initialization

--- app/Engine/Database/Initialization.php
+++ app/Engine/Database/Initialization.php
@@ -5,15 +5,14 @@ function initModel(string $modelName) {
 
     if (DATABASE) {
 
-
-
         require_once APPROOT . '/Engine/Libraries/rb.php';
         require_once APPROOT . '/Engine/Database/Connection.php';
 
         if (!is_array($modelName) && file_exists(APPROOT . "/Models/Model_$modelName.php")) {
-            require_once APPROOT . "/Models/Model_$modelName.php";
-
+            
             \App\Engine\Database\Connection::getInstance();
+
+            require_once APPROOT . "/Models/Model_$modelName.php";
 
             return R::dispense(strtolower($modelName));
         }
