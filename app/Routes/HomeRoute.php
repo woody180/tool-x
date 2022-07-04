cf94f85698544e797c6e0fc2d9a074eb417bf791
woody180 <woody180@rocketmail.com>
2022-07-04 13:30:18 +0400

    update urlSegments function

--- app/Routes/HomeRoute.php
+++ app/Routes/HomeRoute.php
@@ -5,11 +5,3 @@ use App\Engine\Libraries\Router;
 $router = Router::getInstance();
 
 $router->get('/', 'HomeController@index');
-
-
-$router->get('message', function($req, $res) {
-
-    setFlashData('message', 'This is sample message');
-
-    return $res->redirectBack();
-});
