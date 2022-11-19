415dadff189ecd7c2fbd505b0b5f312d68a4e789
woody180 <woody180@rocketmail.com>
2022-11-20 02:07:52 +0400

    update config urls

--- app/Config/urls.php
+++ app/Config/urls.php
@@ -2,6 +2,7 @@
 
 // Application config
 define("URLROOT", "http://localhost:9191");
+//define("URLROOT", (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]");
 define("PUBLIC_DIR", URLROOT . "/assets");
 define("APPROOT", dirname(dirname(__FILE__)));
 
