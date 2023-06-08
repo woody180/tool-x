336caeeb5aa340544ba7e68e19da58e8eba968bd
woody180 <woody180@rocketmail.com>
2023-06-08 12:25:49 +0400

    remove flash message

--- app/Views/welcome.php
+++ app/Views/welcome.php
@@ -4,11 +4,8 @@
 <section class="uk-section">
     <div class="uk-container uk-container-small">
 
-        <?= getFlashData('message') ?>
-        
         <h1><?= $title ?></h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo asperiores beatae officia accusamus quibusdam nobis ipsum repudiandae. Corporis odit voluptatem eveniet modi unde, nam fuga blanditiis harum, ut delectus optio.</p>    
-    
         
     </div>
 </section>
