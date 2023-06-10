99665235dd919b48a71771a6731252aae8e979b4
woody180 <woody180@rocketmail.com>
2023-06-11 03:03:34 +0400

    update html helpers

--- app/Helpers/htmlHelper.php
+++ app/Helpers/htmlHelper.php
@@ -119,3 +119,63 @@ function base64_to_jpeg($base64_string, $output_file) {
     
     return $imagesArray;
 }
+
+
+
+
+function limit_words($string, $limit = 30) {
+    preg_match_all('/[\S]+/m', $string, $resArray);
+
+    if (!empty($resArray[0])) {
+        $wordsCount = count($resArray[0]) + 1;
+        if ($wordsCount > $limit) {
+            $arr = array_splice($resArray[0], 0, $limit);
+            $str = join(' ', $arr);
+
+            return $str . '...';
+        }
+    }
+
+    return $string;
+}
+
+
+
+function get_time_ago($time) {
+    $time_difference = time() - $time;
+
+    if( $time_difference < 1 ) { return 'less than 1 second ago'; }
+    $condition = array( 12 * 30 * 24 * 60 * 60 =>  'year',
+                30 * 24 * 60 * 60       =>  'month',
+                24 * 60 * 60            =>  'day',
+                60 * 60                 =>  'hour',
+                60                      =>  'minute',
+                1                       =>  'second'
+    );
+
+    foreach( $condition as $secs => $str )
+    {
+        $d = $time_difference / $secs;
+
+        if( $d >= 1 )
+        {
+            $t = round( $d );
+            return 'about ' . $t . ' ' . $str . ( $t > 1 ? 's' : '' ) . ' ago';
+        }
+    }
+}
+
+
+
+
+function html2text($html) {
+    
+    $myHtml = strip_tags($html);
+    $converted = strtr($myHtml, array_flip(get_html_translation_table(HTML_ENTITIES, ENT_QUOTES))); 
+    $converted = trim($converted, chr(0xC2).chr(0xA0));
+    $converted  = ltrim(ltrim($converted, '&nbsp;'), ' ');
+    $converted = html_entity_decode($converted);
+    
+    return $converted;
+}
+
